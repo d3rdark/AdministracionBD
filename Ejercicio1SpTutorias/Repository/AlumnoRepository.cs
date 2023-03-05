65cb713903058905d3d55cae50663a02c53adbe4
Ulises Perez <ulisessykes8@gmail.com>
2023-03-04 23:26:02 -0600

    Creacion del entorno de trabajo.

---
+++ Ejercicio1SpTutorias/Repository/AlumnoRepository.cs
@@ -0,0 +1,32 @@
+﻿using Ejercicio1SpTutorias.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1SpTutorias.Repository
+{
+    public class AlumnoRepository : IAlumnoRepository
+    {
+        public void CreateSP(Alumnos entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Alumnos entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Alumnos? GetAlumnoById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Alumnos> GetAlumnos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Ejercicio1SpTutorias/Repository/IAlumnoRepository.cs
@@ -0,0 +1,19 @@
+﻿using Ejercicio1SpTutorias.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1SpTutorias.Repository
+{
+    public interface IAlumnoRepository
+    {
+        public Alumnos? GetAlumnoById(int id);
+        public IEnumerable<Alumnos> GetAlumnos();
+        public void CreateSP(Alumnos entity);
+
+
+        public void Delete(Alumnos entity);
+    }
+}
