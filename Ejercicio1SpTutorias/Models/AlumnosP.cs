7a73c8bf5e7ef7637dd3694c613815797ac0bfd0
Ulises Perez <ulisessykes8@gmail.com>
2023-03-19 13:02:37 -0600

    Models

---
+++ Ejercicio1SpTutorias/Models/AlumnosP.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1SpTutorias.Models
+{
+    public partial class Alumnos
+    {
+        public string? NumeroControlNombre
+        {
+            get
+            {
+                return $"{NumeroControl.ToUpper()} - {Nombre.ToUpper()}";
+            }
+        }
+    }
+}
